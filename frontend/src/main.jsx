711f0a2e3984d828e1f41e1ef9da7f8d6e0cd186
vikal <vikalsaini06@gmail.com>
2026-03-20 14:11:12 +0530

    fix AuthContext import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import AuthContext from "./context/authcontext.jsx";
+// import AuthContext from "./context/Authcontext.jsx";
+import AuthContext from "./context/AuthContext.jsx";
 import UserContext from "./context/UserContext.jsx";
 import ShopContext from "./context/ShopContext.jsx";
 
